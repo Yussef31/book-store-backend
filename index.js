52ff87dfd7f57e808b68902cd60d71134dc95dac
Yussef31 <Youssef.10.chahlaoui@gmail.com>
2025-01-08 19:22:25 +0100

    kk

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ require('dotenv').config()
 //middleware
 app.use(express.json());
 app.use(cors({
-    origin: ['http://localhost:5173','https://book-store-frontend-6pbi.vercel.app'],
+    origin: ['http://localhost:5173'],
     credentials : true
 }));
 
